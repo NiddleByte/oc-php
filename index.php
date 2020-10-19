c3c626815ddd16ec828784e4d1d0a09868db478a
Asep <51188942+NiddleByte@users.noreply.github.com>
2020-10-19 09:15:21 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <html>
 <head> Hello World ! php </head>
   <h1> This is Updated code </h1>
-  <h2> IP :<?php $_SERVER['SERVER_NAME'] ?></h2>
+  <h2> IP :<?php echo exec("/sbin/ifconfig en1 | grep 'inet ' | cut -d ' ' -f2"); ?></h2>
 </html>
